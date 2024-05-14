cc47f6df3a824636372f39ad94762a8a4a0d5e88
Lucas Nagle <lnagle@gmail.com>
2024-05-13 21:06:10 -0400

    Add support for dynamic prompts from past responses

--- emotional-reasoning/index.py
+++ emotional-reasoning/index.py
@@ -9,7 +9,7 @@ MULTI_RESPONSE = 'MULTI_RESPONSE'
 steps = [
   {
     'id': 1,
-    'prompt': 'What\'s on your mind?',
+    'prompt': 'What\'s on your mind?\n',
     'type': SINGLE_ANSWER
   },
   {
@@ -17,27 +17,27 @@ steps = [
     'prompt': 'How is that making you feel?',
     'type': MULTI_RESPONSE
   },
-  # {
-  #   'id': 3,
-  #   'prompt': 'Why do you feel {emotion}?',
-  #   'type': MULTI_RESPONSE,
-  #   'repeat_for_each_response_from': 2
-  # },
-  # {
-  #   'id': 4,
-  #   'prompt': 'Can you question or challenge these thoughts?',
-  #   'type': MULTI_RESPONSE,
-  # },
-  # {
-  #   'id': 5,
-  #   'prompt': 'Are there any specific actions you\'d like to take?',
-  #   'type': MULTI_RESPONSE
-  # }, 
-  # {
-  #   'id': 6,
-  #   'prompt': 'How do you feel now?',
-  #   'type': SINGLE_ANSWER
-  # }
+  {
+    'id': 3,
+    'prompt': 'Why do you feel {0}?',
+    'type': MULTI_RESPONSE,
+    'repeat_for_each_response_from': 2
+  },
+  {
+    'id': 4,
+    'prompt': 'Can you question or challenge these thoughts?',
+    'type': MULTI_RESPONSE,
+  },
+  {
+    'id': 5,
+    'prompt': 'Are there any specific actions you\'d like to take?',
+    'type': MULTI_RESPONSE
+  }, 
+  {
+    'id': 6,
+    'prompt': 'How do you feel now?\n',
+    'type': SINGLE_ANSWER
+  }
 ]
 
 question_response_map = {
@@ -59,18 +59,28 @@ def loop_of_inputs(prompt):
 
   return responses
 
-def handle_question_type(step):
+def handle_question_type(step, additional_prompt_args = []):
   if step['type'] == SINGLE_ANSWER:
-    response = input(step['prompt'])
+    response = input(step['prompt'].format(*additional_prompt_args))
     return response
   elif step['type'] == MULTI_RESPONSE:
-    responses = loop_of_inputs(step['prompt'])
+    responses = loop_of_inputs(step['prompt'].format(*additional_prompt_args))
     return responses
 
 def main(steps):
   for i in range(len(steps)):
     step = steps[i] 
-    response = handle_question_type(step)
-    question_response_map[step['id']] = response
 
-main(steps)    
+    if 'repeat_for_each_response_from' in step:
+      related_responses = question_response_map[step['repeat_for_each_response_from']]
+      responses = []
+
+      for j in range(len(related_responses)):
+        response = handle_question_type(step, related_responses[j])
+        responses.append(response)
+        question_response_map[step['id']] = responses
+    else:    
+      response = handle_question_type(step)
+      question_response_map[step['id']] = response
+
+main(steps)
