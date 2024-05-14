dd6bc0fc13f22aa567f06e5d12226608306fc18f
Lucas Nagle <lnagle@gmail.com>
2024-05-13 20:52:24 -0400

    Begin moving away from a hardcoded flow of questions to using an abstract flowchart data structure

--- emotional-reasoning/index.py
+++ emotional-reasoning/index.py
@@ -3,6 +3,47 @@ TODO
   Capture stdout as a separate variable
 '''
 
+SINGLE_ANSWER = 'SINGLE_ANSWER'
+MULTI_RESPONSE = 'MULTI_RESPONSE'
+
+steps = [
+  {
+    'id': 1,
+    'prompt': 'What\'s on your mind?',
+    'type': SINGLE_ANSWER
+  },
+  {
+    'id': 2,
+    'prompt': 'How is that making you feel?',
+    'type': MULTI_RESPONSE
+  },
+  # {
+  #   'id': 3,
+  #   'prompt': 'Why do you feel {emotion}?',
+  #   'type': MULTI_RESPONSE,
+  #   'repeat_for_each_response_from': 2
+  # },
+  # {
+  #   'id': 4,
+  #   'prompt': 'Can you question or challenge these thoughts?',
+  #   'type': MULTI_RESPONSE,
+  # },
+  # {
+  #   'id': 5,
+  #   'prompt': 'Are there any specific actions you\'d like to take?',
+  #   'type': MULTI_RESPONSE
+  # }, 
+  # {
+  #   'id': 6,
+  #   'prompt': 'How do you feel now?',
+  #   'type': SINGLE_ANSWER
+  # }
+]
+
+question_response_map = {
+  
+}
+    
 def loop_of_inputs(prompt):
   responses = []
   last_response = ''
@@ -18,14 +59,18 @@ def loop_of_inputs(prompt):
 
   return responses
 
-emotions = loop_of_inputs('What are you feeling?')
+def handle_question_type(step):
+  if step['type'] == SINGLE_ANSWER:
+    response = input(step['prompt'])
+    return response
+  elif step['type'] == MULTI_RESPONSE:
+    responses = loop_of_inputs(step['prompt'])
+    return responses
 
-for emotion in emotions:
-  emotion_prompt = 'Why are you feeling {emotion}?'.format(emotion=emotion)
-  loop_of_inputs(emotion_prompt)
-  
-action = input('Are there any actions you\'d like to take?\n')
+def main(steps):
+  for i in range(len(steps)):
+    step = steps[i] 
+    response = handle_question_type(step)
+    question_response_map[step['id']] = response
 
-summary = input('Briefly summarize this session and how you\'re feeling now.\n')
-
-print('Exiting')
+main(steps)    
